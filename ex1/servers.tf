fe36c31b908847e6ccdcaa9975c57de309932260
mohi <mophindhar.mukku@outlook.com>
2023-06-01 18:55:59 +0530

    creating the instances

--- ex1/servers.tf
+++ ex1/servers.tf
@@ -2,55 +2,55 @@
 data "aws_ami" "centos" {
   owners      = ["973714476881"]
   most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
+  name_regex  = "Centos-8-DevOps-Practice" # ami name should be same as the image name
 }
 
-output "aws_ami" {
+output "ami_image_id" {
   value = data.aws_ami.centos.image_id
 }
 
 
-#resource "aws_instance" "frontend" {
-#  ami           = "ami-0b5a2b5b8f2be4ec2"
-#  instance_type = "t3.micro"
-#
-#  tags = {
-#    Name = "frontend"
-#  }
-#}
-#
-#resource "aws_instance" "mongodb" {
-#  ami           = "ami-0b5a2b5b8f2be4ec2"
-#  instance_type = "t3.micro"
-#
-#  tags = {
-#    Name = "mongodb"
-#  }
-#}
-##reperate this block for creating all the 10 instances
-#resource "aws_instance" "catalogue" {
-#  ami           = "ami-0b5a2b5b8f2be4ec2"
-#  instance_type = "t3.micro"
-#
-#  tags = {
-#    Name = "catalogue"
-#  }
-#}
-#
-#resource "aws_instance" "redis" {
-#  ami           = "ami-0b5a2b5b8f2be4ec2"
-#  instance_type = "t3.micro"
-#
-#  tags = {
-#    Name = "redis"
-#  }
-#}
-##reperate this block for creating all the 10 instances
-#resource "aws_instance" "user" {
-#  ami           = "ami-0b5a2b5b8f2be4ec2"
-#  instance_type = "t3.micro"
-#
-#  tags = {
-#    Name = "user"
-#  }
-#}
+resource "aws_instance" "frontend" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+resource "aws_instance" "mongodb" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "mongodb"
+  }
+}
+#reperate this block for creating all the 10 instances
+resource "aws_instance" "catalogue" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "catalogue"
+  }
+}
+
+resource "aws_instance" "redis" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "redis"
+  }
+}
+#reperate this block for creating all the 10 instances
+resource "aws_instance" "user" {
+  ami           = "data.aws_ami.centos.image_id"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "user"
+  }
+}
