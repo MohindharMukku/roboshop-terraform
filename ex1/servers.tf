4ae113fa2c82cbe6607ae1133a9cdf6bc0e66a47
mohi <mophindhar.mukku@outlook.com>
2023-06-01 18:43:22 +0530

    ami_id output

--- ex1/servers.tf
+++ ex1/servers.tf
@@ -2,7 +2,7 @@
 data "aws_ami" "centos" {
   owners      = ["973714476881"]
   most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
+  name_regex  = "Centos-8-DevOps-practice"
 }
 
 output "aws_ami" {
