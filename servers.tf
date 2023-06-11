4b50f10898d1e5e94881bb252089e5a7c55301c2
mohi <mophindhar.mukku@outlook.com>
2023-06-11 18:13:04 +0530

    added the state.tf file and added the ".terraform.lock.hcl" file
    updated the version

--- servers.tf
+++ servers.tf
@@ -7,10 +7,10 @@
 #  }
 #}
 #
-## Configure the AWS Provider
-#provider "aws" {
-#  region = "us-east-1"
-#}
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
 
 #reperate this block for creating all the 10 instances
 data "aws_ami" "centos" {
