42cdaa54b80278ab16502d7ae4389d9d582ae814
mohi <mohindhar.mukku@outlook.com>
2023-06-14 23:03:04 +0530

    testing the code without the provider block

--- servers.tf
+++ servers.tf
@@ -1,16 +1,16 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-#
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-}
+#terraform {
+#  required_providers {
+#    aws = {
+#      source  = "hashicorp/aws"
+#      version = "~> 5.0"
+#    }
+#  }
+#}
+##
+## Configure the AWS Provider
+#provider "aws" {
+#  region = "us-east-1"
+#}
 
 #reperate this block for creating all the 10 instances
 data "aws_ami" "centos" {
