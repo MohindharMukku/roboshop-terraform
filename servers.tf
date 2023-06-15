af539642b40f972d9ad93b7cc046c814e8a65168
mohi <mohindhar.mukku@outlook.com>
2023-06-15 19:09:44 +0530

    1. pushing the as per the session -27, 2. jenkins with the multiple environment 3. adding env tag to the instance

--- servers.tf
+++ servers.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "instances" {
   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
   tags = {
-    Name = each.value["name"]
+    Name = f"{each.value['name']}_{env}"
   }
 }
 
