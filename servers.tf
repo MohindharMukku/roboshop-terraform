5f8ed2a894ee85b856084978ce35c2277751c55d
mohi <mohindhar.mukku@outlook.com>
2023-06-26 16:46:27 +0530

    tf code with the module - s31, spelling mistake modules- "s" missing

--- servers.tf
+++ servers.tf
@@ -42,7 +42,7 @@
 
 module "database-servers" {
   for_each       = var.database_servers
-  source         = "./module"
+  source         = "./modules"
   component_name = each.value["name"]
   env            = var.env
   instance_type  = each.value["instance_type"]
@@ -53,7 +53,7 @@ module "database-servers" {
 module "app-servers" {
   depends_on = [module.database-servers]
   for_each        = var.apps_servers
-  source          = "./module"
+  source          = "./modules"
   component_name  = each.value["name"]
   env             = var.env
   instance_type   = each.value["instance_type"]
