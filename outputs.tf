af1aa731016714d7179e05c3bd5be10b19b36646
mohi <mohindhar.mukku@outlook.com>
2023-06-26 17:08:31 +0530

    tf code with the module - s29, uncommiting the output.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-output "ami_image_id" {
-  value = data.aws_ami.centos.image_id
-}
-
-output "DNS_name" {
-  value = aws_route53_record.frontend.name
-}
+#output "ami_image_id" {
+#  value = data.aws_ami.centos.image_id
+#}
+#
+#output "DNS_name" {
+#  value = aws_route53_record.frontend.name
+#}
