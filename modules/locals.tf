e802595d7216dc1f64d5f9122f92266b13a5ecb9
mohi <mohindhar.mukku@outlook.com>
2023-07-06 16:18:24 +0530

    segregated the db and app cmnds

--- modules/locals.tf
+++ modules/locals.tf
@@ -1,3 +1,16 @@
 locals {
   name = var.env != "" ? "${var.env}-${var.component_name}" : var.component_name
+  
+  db_commands = [
+    "rm -rf roboshop-shell",
+    "git clone https://github.com/MohindharMukku/roboshop-shell",
+    "cd roboshop-shell",
+    "sudo bash ${var.component_name}.sh ${var.password}"
+  ]
+  
+  app_commands = [
+    "sudo autolab ansible",
+    "ansible-pull -i localhost, -U https://github.com/MohindharMukku/roboshop-ansible roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
+  ]
+  
 }
