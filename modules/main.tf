b8786f1901d68c880440266ae30ddc2e8d7a583a
mohi <mohindhar.mukku@outlook.com>
2023-07-07 18:45:20 +0530

    error fixed in ssm-ps-policy

--- modules/main.tf
+++ modules/main.tf
@@ -93,7 +93,6 @@ resource "aws_iam_role_policy" "ssm-ps-policy" {
         "Effect": "Allow",
         "Action": [
           "kms:Encrypt",
-          "kms:Decrypt",
           "ssm:GetParameterHistory",
           "ssm:GetParametersByPath",
           "ssm:GetParameters",
@@ -101,7 +100,7 @@ resource "aws_iam_role_policy" "ssm-ps-policy" {
         ],
         "Resource": [
           "arn:aws:kms:us-east-1:655263643424:key/arn:aws:kms:us-east-1:655263643424:key/f4069cfc-ff14-4bf8-b002-0f5b56727881",
-          "arn:aws:ssm:us-east-1:655263643424:parameter/${var.env}-${var.component_name}.*"
+          "arn:aws:ssm:us-east-1:655263643424:parameter/${var.env}.${var.component_name}.*"
         ]
       }
     ]
